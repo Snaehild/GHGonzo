3647ae5293647face0b504e57dc54bcecb0f6686
Snaehild <Snaehild@users.noreply.github.com>
2014-08-09 12:41:39 +0800

    1.9.0

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<section id="omc-sidebar" class="omc-right">
+
+<a href="#top" class="omc-mobile-back-to-top"><?php _e('Back to Top', 'gonzo');?> ↑</a>
+
+<ul class="xoxo">
+<?php
+dynamic_sidebar(get_sidebar_name());
+?>	
+
+</ul><!-- /xoxo -->
+
+</section>	
+
+<br class="clear" />
+
+</div> <!-- end of #container -->
